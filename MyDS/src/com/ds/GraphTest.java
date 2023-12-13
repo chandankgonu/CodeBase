c617a3b33a8d49c55e502756c54413f59f20ed35
chandan kumar <chandan.kgonu@gmail.com>
2023-12-13 09:05:32 +0530

    create graph

--- MyDS/src/com/ds/GraphTest.java
+++ MyDS/src/com/ds/GraphTest.java
@@ -1,5 +1,7 @@
 package com.ds;
 
+import java.util.ArrayList;
+
 public class GraphTest {
     
     class Edge{
@@ -11,10 +13,50 @@ public class GraphTest {
             this.dest = dest;
         }
     }
-    
+    //create graph
+
+    private static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0; i< graph.length;i++)
+           graph[i] = new ArrayList<>();  
+
+    }
+
+    //add edge in graph
+    private void addEdge(ArrayList<Edge> graph[], int sour, int dist){
+        graph[sour].add(new Edge(sour, dist));
+    }
+    //print graph
+
+    private static void printGraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+          for(int j=0; j< graph[i].size();j++){
+            Edge edge = graph[i].get(j);
+
+            System.out.println("Source : "+ edge.source + "  Distination : "+ edge.dest);
+          }
+        }
+    }
     
     public static void main(String[] args) {
+        ArrayList<Edge> graph[] = new ArrayList[5];
+        createGraph(graph);
+
+        GraphTest gt = new GraphTest();
+        gt.addEdge(graph, 0, 2);
+        gt.addEdge(graph, 1, 2);
+        gt.addEdge(graph, 1, 3);
+
+
+        gt.addEdge(graph, 2, 0);
+        gt.addEdge(graph, 2, 1);
+        gt.addEdge(graph, 2, 3);
+
+        gt.addEdge(graph, 3, 1);
+        gt.addEdge(graph, 3, 2);
+
+        printGraph(graph);
         
+
     }
     
 }
